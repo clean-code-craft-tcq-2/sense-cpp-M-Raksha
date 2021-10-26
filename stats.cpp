cfcdcb26d78541bc5734e6f65605ef61c10c0b87
M-Raksha <91051247+M-Raksha@users.noreply.github.com>
2021-10-26 20:05:39 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,17 @@
 #include "stats.h"
+#include "math.h"
+#include "algorithm"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+Stats Statistics::ComputeStatistics(const std::vector<float>&vect ) {
+    Stats Calcualtion;
+    float sum = 0.0f;
+    for (int i=0; i<vect.size(); i++)
+    {
+        sum = sum+vect[i];
+    }
+    Calcualtion.average = sum/vect.size();
+    Calcualtion.min=*min_element(vect.begin(), vect.end());
+    Calcualtion.max =*max_element(vect.begin(), vect.end());
+    
+    return Calcualtion;
 }
