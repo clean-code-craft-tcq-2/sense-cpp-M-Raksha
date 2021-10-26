6d084655da5818b21ba99ec8061b0b291e12bc92
M-Raksha <91051247+M-Raksha@users.noreply.github.com>
2021-10-26 19:31:44 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,5 +1,11 @@
 #include <vector>
 
+class stats
+{
+    float Avg = 0f;
+    float Max = 0f;
+    float Min = 0f;
+ };
 namespace Statistics {
-    Stats ComputeStatistics(const std::vector<___>& );
+    Stats ComputeStatistics(const std::vector<float>&Vector_elements);
 }
