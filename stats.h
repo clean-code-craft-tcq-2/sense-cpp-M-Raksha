23459b4b597deb9faa412fd3cc972e217a73abe4
M-Raksha <91051247+M-Raksha@users.noreply.github.com>
2021-10-27 11:37:21 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -10,3 +10,29 @@ class Stats
 namespace Statistics {
     Stats ComputeStatistics(const std::vector<float>&Vector_elements);
 }
+class IAlerter
+{
+   public:
+    bool emailSent;
+    bool ledGlows;
+
+};
+
+class EmailAlert:public IAlerter
+{
+  
+};  
+
+class LEDAlert :public IAlerter
+{
+   
+};
+class StatsAlerter
+{
+    private:
+    float Threshold_local;
+    std::vector<IAlerter*>Alert_local;
+    public:
+    StatsAlerter(float Threshold,std::vector<IAlerter*>&Alert);
+    void checkAndAlert(const std::vector<float>&vect);
+};
